b9e1997db29e67fef279286c6050d54dce160052
Rachel Dilley <racheldilley11@gamil.com>
2021-03-21 20:51:02 -0400

    add new model with decreased classes

---
+++ Notebooks/ImageFunctions.py
@@ -0,0 +1,175 @@
+import pandas as pd
+import numpy as np
+
+# color distributions
+import cv2
+import imutils
+
+#img show 
+import matplotlib.pyplot as plt
+from matplotlib import image
+
+#img load
+import requests
+# !pip install fake_useragent
+from fake_useragent import UserAgent 
+from io import BytesIO
+from PIL import Image
+
+def histogram(image, mask, bins):
+    # extract a 3D color histogram from the masked region of the image, using the supplied number of bins per channel
+    hist = cv2.calcHist([image], [0,1,2], mask, [bins[0],bins[1],bins[2]],[0, 180, 0, 256, 0, 256])
+    
+    # normalize the histogram if we are using OpenCV 2.4
+    if imutils.is_cv2():
+        hist = cv2.normalize(hist).flatten()
+        
+    # otherwise handle for OpenCV 3+
+    else:
+        hist = cv2.normalize(hist, hist).flatten()
+
+    return hist
+
+def get_color_description(img_array, bins, color):
+    img = img_array * 255
+    image = cv2.cvtColor(img, color)
+    
+    features = []
+   
+    # grab the dimensions and compute the center of the image
+    (h, w) = image.shape[:2]
+    (cX, cY) = (int(w * 0.5), int(h * 0.5))
+
+    # divide the image into four rectangles/segments (top-left, top-right, bottom-right, bottom-left)
+    segments = [(0, cX, 0, cY), (cX, w, 0, cY), (cX, w, cY, h), (0, cX, cY, h)]
+
+    # construct an elliptical mask representing the center of the image
+    (axesX, axesY) = (int(w * 0.75) // 2, int(h * 0.75) // 2)
+    ellipMask = np.zeros(image.shape[:2], dtype = "uint8")
+    cv2.ellipse(ellipMask, (cX, cY), (axesX, axesY), 0, 0, 360, 255, -1)
+
+    # loop over the segments
+    for (startX, endX, startY, endY) in segments:
+        # construct a mask for each corner of the image, subtracting the elliptical center from it
+        cornerMask = np.zeros(image.shape[:2], dtype = "uint8")
+        cv2.rectangle(cornerMask, (startX, startY), (endX, endY), 255, -1)
+        cornerMask = cv2.subtract(cornerMask, ellipMask)
+
+        # extract a color histogram from the image, then update the feature vector
+        hist = histogram(image, cornerMask,bins)
+        features.extend(hist)
+
+        # extract a color histogram from the elliptical region and update the feature vector
+        hist = histogram(image, ellipMask, bins)
+        features.extend(hist)
+    return features
+
+def plot_color_hist(image, bins, mask, name):
+
+    rgb    = cv2.split(image)
+
+    plt.figure(figsize = (10,5))
+    colors = ['r','g','b']
+    
+    for i,col in enumerate(colors):
+        arr = rgb[i] * mask
+        arr = arr.flatten()
+        plt.hist(list(arr), bins, [1,256], color=col) 
+
+        plt.xlim([0,256])
+        
+    #save
+    if name != None:
+        path = '../Graphs/' + 'color_hist_bottom_right' + '.png'
+        plt.savefig(path)
+    
+    plt.show()
+
+def show_image(img):
+    
+    imgplot = plt.imshow(img)
+    plt.axis('off')
+    plt.grid(False)
+    plt.show()
+    
+def load_image(url):
+    ua = UserAgent()
+    headers = {'user-agent': ua.random}
+
+    response = requests.get(url, headers = headers)
+    image_io = BytesIO(response.content)
+    img = Image.open(image_io)    
+    return np.array(img)
+
+def load_image_streamlit(url):
+    ua = UserAgent()
+    headers = {'user-agent': ua.random}
+
+    response = requests.get(url, headers = headers)
+    image_io = BytesIO(response.content)
+    img = Image.open(image_io)    
+    return img
+    
+def show_save_image(img, name):
+    # show
+    plt.rcParams['figure.figsize']=(4,4)
+    imgplot = plt.imshow(img)
+    plt.axis('off')
+    plt.grid(False)
+    plt.show()
+    
+    #save
+    path = '../Graphs/' + name + '.png'
+    image.imsave(path, img)
+    
+def show_images(img_list, category):
+    plt.figure(figsize = (25,15));
+    t = f"Showing from {category} category"
+    
+    plt.suptitle(t, fontsize=20)
+    for index, img in enumerate(img_list):
+        plt.subplot(10,10,index+1);
+        plt.imshow(img)
+        plt.axis('off')
+
+def get_categories_top_bottom_imgs(df, categories, num):
+    grouped = df.groupby('category')
+    group_centroid = df.groupby("category")["color_feats"].apply(lambda x: np.array(x.tolist()).mean(axis=0))
+
+    firstvals = {}
+    lastvals = {}
+
+    
+    for idx,cat in enumerate(categories):
+        group = grouped.get_group(cat) 
+
+        cent = group_centroid[cat]
+
+        sorted_dist = get_sorted_distances_list(group, cent)
+        
+        firstvals[cat] = [k for k in list(sorted_dist.keys())[:num]]
+        lastvals[cat] = [k for k in list(sorted_dist.keys())[-num:]]
+    
+    return (firstvals, lastvals)
+
+def get_sorted_distances_list(df, feat):
+    distances = {}
+    for index, row in df.iterrows():
+        pt = np.array(row[4])
+        dist = np.linalg.norm(feat-pt)
+        distances[index] = dist
+
+    # distances
+    sorted_dist = dict(sorted(distances.items(), key=lambda item: item[1]))
+    
+    return sorted_dist
+
+def remove_bottom_imgs(X, df, categories, bottom_imgs_dict, remove):
+    remove_indexes = remove
+    for cat in categories:
+        remove_indexes = remove_indexes + bottom_imgs_dict[cat]
+        
+    df.drop(df.index[remove_indexes], inplace = True)
+    X = np.delete(X, remove_indexes, 0)
+    
+    return X, df
--- Notebooks/WebApp.py
+++ Notebooks/WebApp.py
@@ -2,9 +2,12 @@ import streamlit as st
 
 import pandas as pd
 import numpy as np
+import pickle
 
 # img load
 from PIL import Image
+
+from ImageFunctions import load_image_streamlit
 
 # models
 from keras.models import load_model, Model
@@ -14,9 +17,23 @@ import keras
 #load model
 model = load_model('../Models/vgg_cnn.h5')
 
-#load img df
-# df = pd.read_pickle('../Data/img_info_df.pkl')
-# grouped = df.groupby('category')
+# color distributions
+import cv2
+import imutils
+
+inputs = (150, 150, 3)
+vgg = vgg16.VGG16(include_top=False, weights='imagenet', 
+                                     input_shape=inputs)
+
+output = vgg.layers[-1].output
+output = keras.layers.Flatten()(output)
+vgg_model = Model(vgg.input, output)
+
+#dont want model weights to change durring training
+vgg_model.trainable = False
+for layer in vgg_model.layers:
+    layer.trainable = False
+
 
 def classify(image, vgg_model, model):
     cats = ['art', 'beaches/ocean', 'entertainment', 'gardens/zoo', 'landmarks', 'museums',
@@ -82,40 +99,43 @@ def get_color_description(img_array, bins, color):
 def get_sorted_distances_list(df, feat):
     distances = {}
     for index, row in df.iterrows():
-        pt = np.array(row[4])
+        pt = np.array(row[5])
+        # print(pt)
         dist = np.linalg.norm(feat-pt)
         distances[index] = dist
 
     # distances
-    sorted_dist = dict(sorted(distances.items(), key=lambda item: item[1]))
+    sorted_dist = dict(sorted(distances.items(), key=lambda item: item[1], reverse=False))
     
     return sorted_dist
 
-def find_closest_imgs(img_class, img):
-    img_df = grouped.get_group(img_class)
+def find_closest_imgs(img_class, img, img_df):
 
     bins = [8,8,8]
     color = cv2.COLOR_BGR2HSV
-    feats = get_color_description(x, bins, color)
+    feats = get_color_description(img, bins, color)
+    feats = np.array(feats)
 
     sorted_dist = get_sorted_distances_list(img_df, feats)
 
     attractions = []
     urls = []
     locations = []
-    while len(locations) < 3:
+    x = 1
+    while x < 3:
         for key, value in sorted_dist.items():
             if img_df.loc[key,'name'] not in attractions:
                 attractions.append(img_df.loc[key,'name'])
                 urls.append(img_df.loc[key,'url'])
                 locations.append(img_df.loc[key,'location'])
+                x = x+1
     
     df = pd.DataFrame()
     df['name'] = attractions
     df['url'] = urls
     df['location'] = locations
 
-    return df
+    return df[:3]
 
 def get_bottleneck_features(model, input_img):
     input_imgs = np.array([input_img])
@@ -123,21 +143,29 @@ def get_bottleneck_features(model, input_img):
     features = model.predict(input_imgs, verbose=0)
     return features
 
-inputs = (150, 150, 3)
-vgg = vgg16.VGG16(include_top=False, weights='imagenet', 
-                                     input_shape=inputs)
-
-output = vgg.layers[-1].output
-output = keras.layers.Flatten()(output)
-vgg_model = Model(vgg.input, output)
-
-#dont want model weights to change durring training
-vgg_model.trainable = False
-for layer in vgg_model.layers:
-    layer.trainable = False
 
 
-st.title('US Tourist Attraction Recommender')
+def get_recommendations(img_class, img_array):
+    #load df with color descriptions
+    file_name = img_class.replace('/', '_')
+    st.write(file_name)
+    path = '/Users/racheldilley/Documents/lets-take-a-trip-data/AppData/' + file_name + '_df.pkl'
+    df = pickle.load(open(path, 'rb'))
+    df = df[:1000]
+    # df = pd.read_pickle(path)
+    st.write(df.head())
+
+    top_df = find_closest_imgs(img_class, img_array, df)
+    return top_df
+
+def show_recommendations(df):
+    for index, row in df.iterrows():
+        img = load_image_streamlit(row[1])
+        st.image(img, width=300)
+
+
+
+st.title('US Tourist Attraction Recommende')
 #st.image()
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 if uploaded_file is not None:
@@ -148,8 +176,11 @@ if uploaded_file is not None:
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
     st.write("Recommending...")
+
     label = classify(img_array, vgg_model, model)
     st.write(label)
+    df = get_recommendations(label, img_array)
+    show_recommendations(df)
     # closest_imgs = find_closest_imgs(label, img_array)
     # st.write(closest_imgs)
 
